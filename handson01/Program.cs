ff6c7e4aee035f0e51504d08d6d4895ad8915e51
shawnswrld <shawnswrld@gmail.com>
2017-06-06 12:51:48 -0400

    neww1

--- handson01/Program.cs
+++ handson01/Program.cs
@@ -1,40 +1,64 @@
 ﻿using System;
+using System.Xml.Linq;
 
 // Set the namespace to be HandsOn - note the capital H and O
-namespace handson01
+namespace Handson01
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args);
+        
             // extra { not needed
             //{
                 int x = 3;
                 int y = 4;
                 int sum = 7;
 
-                // Define an integer variable named sum that is initialized using an expression that produce the sum of variables x and y.
-                // So not a bool type, but an int type here.  Don't forget your semi-colons
-                bool sum = true
- 
+            // Define an integer variable named sum that is initialized using an expression that produce the sum of variables x and y.
+            // So not a bool type, but an int type here.  Don't forget your semi-colons
+            int sum = (x + y);
+        DoWork("sum");
+
+
+
 
         // extra } not needed
         //}
 
-            // extra { not needed
-            //{ 
-            
-            // text1 and text2 should be a string type.  Strings must be surrounded with the double quotes "
-            int text1 = Hello;
-            int text2 = World;
+        // extra { not needed
+        //{ 
+
+        // text1 and text2 should be a string type.  Strings must be surrounded with the double quotes "
+        int text1 = "Hello" + int public int Text1 { get => Text11; set => (Text11 = value); }
+
+
+        public int Text11
+        {
+            get
+            {
+            }
+
+            set
+            {
+            }
+        }
+
+        public int Text12 { get => Text14; set => Text14 = value; }
+        public int Text13 { get => Text14; set => Text14 = value; }
+        public int Text14 { get => text1; set => text1 = value; }
+
+        XText = "World";
 
             // the message variable should be defined, it was only initialized and the type is unknown
             message = ("text1" + "text2")
-            return message
+            
             
             // Any code after a return statement (line 33) will never run.  But sure to have all code appear before the return statement.
             // The output should be the concatentation of text1 and text2, you have hard coded "hello world" below.
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); 
+            return message
+
+
         }
     }
 }
