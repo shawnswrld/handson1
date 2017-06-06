82b978146d180e8ace3fcfc7cf3ece2a2d63989a
Jason Tennant <xcodula@icloud.com>
2017-06-06 09:06:53 -0700

    adding code review

--- handson01/Program.cs
+++ handson01/Program.cs
@@ -1,25 +1,47 @@
 ﻿using System;
 
+// Set the namespace to be HandsOn - note the capital H and O
 namespace handson01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            {
+            // extra { not needed
+            //{
                 int x = 3;
                 int y = 4;
                 int sum = 7;
 
+                // Define an integer variable named sum that is initialized using an expression that produce the sum of variables x and y.
+                // So not a bool type, but an int type here.  Don't forget your semi-colons
                 bool sum = true
  
 
-        }
-            { int text1 = Hello;
-                int text2 = World;
-                message = ("text1" + "text2")
-                return message
-                Console.WriteLine("Hello World!");
+        // extra } not needed
+        //}
+
+            // extra { not needed
+            //{ 
+            
+            // text1 and text2 should be a string type.  Strings must be surrounded with the double quotes "
+            int text1 = Hello;
+            int text2 = World;
+
+            // the message variable should be defined, it was only initialized and the type is unknown
+            message = ("text1" + "text2")
+            return message
+            
+            // Any code after a return statement (line 33) will never run.  But sure to have all code appear before the return statement.
+            // The output should be the concatentation of text1 and text2, you have hard coded "hello world" below.
+            Console.WriteLine("Hello World!");
         }
     }
 }
+
+// Things that are missing:
+// 
+// In the program Main entry point method, add a single-line comment that contains the text: 
+// "Main method demonstrates the use of variables, operators, expressions, and statements".
+//
+// 
