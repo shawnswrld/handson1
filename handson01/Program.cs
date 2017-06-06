7a801ee244f242550405839843f5491ba793f739
shawnswrld <shawnswrld@gmail.com>
2017-06-06 11:45:34 -0400

    new

---
+++ handson01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace handson01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int x = 3;
+                int y = 4;
+                int sum = 7;
+
+                bool sum = true
+ 
+
+        }
+            { int text1 = Hello;
+                int text2 = World;
+                message = ("text1" + "text2")
+                return message
+                Console.WriteLine("Hello World!");
+        }
+    }
+}
